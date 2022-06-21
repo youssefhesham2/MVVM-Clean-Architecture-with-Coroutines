cc66d98c7ed8fc364b30e997a1d0029ee23b06cf
youssef hesham <64506232+youssefhesham2@users.noreply.github.com>
2022-06-21 23:31:13 +0200

    Move the base URL to BuildConfig
    to make the code more readable

--- data/src/main/java/com/example/data/datasources/remote/NetworkModule.kt
+++ data/src/main/java/com/example/data/datasources/remote/NetworkModule.kt
@@ -1,15 +1,16 @@
 package com.example.data.datasources.remote
 
+import com.example.data.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class NetworkModule {
     companion object {
-        private  var ApiClient: UserApiService? = null
+        private var ApiClient: UserApiService? = null
         fun apiClient(): UserApiService {
             ApiClient?.let { ApiClient } ?: let {
                 ApiClient = Retrofit.Builder()
-                    .baseUrl("https://5e510330f2c0d300147c034c.mockapi.io/")
+                    .baseUrl(BuildConfig.USER_BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
                     .create(UserApiService::class.java)
